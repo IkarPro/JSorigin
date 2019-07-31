12ea8d8c8833126d67fa207321a8b0dce2255a9f
Irakliy <ikar06@mail.ru>
2019-07-31 16:14:26 +0300

    Второе занятие

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
 'use strict';
 
-let money,
-    time,
-    expenses = {};
 
-money = prompt('Your monthly budget?');
-time = prompt('Your date in format - YYYY-MM-DD');
-
-let arrExpenses = [prompt('Obyazatelnaya statiya rashodov'),
-    prompt('Price'),
-    prompt('Obyazatelnaya statiya rashodov'),
-    prompt('Price')];
-expenses[arrExpenses[0]] = arrExpenses[1];
-expenses[arrExpenses[2]] = arrExpenses[3];
+let money = +prompt('Your monthly budget?'),
+    time = prompt('Your date in format - YYYY-MM-DD');
 
 let appData = {
     budget : money,
     timeData : time,
-    expensesData : expenses,
-    optionalExpensesData : {},
+    expenses : {},
+    optionalExpenses : {},
     income : [],
     savings : false
 };
 
-alert('Budget na odin den - ' + money/30);
+/*for (let i = 0; i < 2; i++) {
+    let a = prompt('Obyazatelnaya statiya rashodov'),
+        b = prompt('Price');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50){
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+}*/
+
+/*let i = 0;
+while (i<2) {
+    let a = prompt('Obyazatelnaya statiya rashodov'),
+        b = prompt('Price');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50){
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+    i++;
+}*/
+
+let i = 0;
+do {
+    let a = prompt('Obyazatelnaya statiya rashodov'),
+        b = prompt('Price');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50){
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+    i++;
+} while (i < 2)
+
+appData.moneyPerDay = appData.budget/30;
+
+if (appData.moneyPerDay <= 100){
+    console.log('minimum');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log('norm');
+} else if (appData.moneyPerDay >= 2000){
+    console.log('high');
+} else {
+    console.log('error');
+}
+
+alert(appData.moneyPerDay);
