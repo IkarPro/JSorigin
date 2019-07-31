940d1dbc89f487418a680d5e93fee27b476f3207
Irakliy <ikar06@mail.ru>
2019-07-31 12:14:20 +0300

    Исправление ошибок

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-var arr = ['первый', 'второй'];
+'use strict';
 
-console.log(arr['0']);
+let money,
+    time,
+    expenses = {};
+
+money = prompt('Your monthly budget?');
+time = prompt('Your date in format - YYYY-MM-DD');
+let arrExpenses = [prompt('Obyazatelnaya statiya rashodov'),
+    prompt('Price'),
+    prompt('Obyazatelnaya statiya rashodov'),
+    prompt('Price')];
+expenses[arrExpenses[0]] = arrExpenses[1];
+expenses[arrExpenses[2]] = arrExpenses[3];
+console.log(expenses);
+let appData = {
+    budget : money,
+    timeData : time,
+    expensesData : expenses,
+    optionalExpensesData : {},
+    income : [],
+    savings : false
+};
+console.log(appData);
+alert('Budget na odin den - ' + money/30);
